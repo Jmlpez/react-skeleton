17632b0eb25f2af5f514800917ea052e6d5f550e
Jesus <jmlopezponce@gmail.com>
2025-04-07 15:30:59 -0400

    add utility functions for enum handling, sleep simulation, and nameof operator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,6 +12,48 @@ export const cn = (...inputs: ClassValue[]) => {
 };
 
 /**
+ * Get list of string from members (keys) of a given Enum
+ *
+ * @param enumType Enum Type
+ * @returns Enum members as a list of strings
+ */
+export const getEnumKeys = (enumType: Record<string, string | number>): Array<string> =>
+    // https://github.com/microsoft/TypeScript/issues/17198
+    // https://www.angularjswiki.com/angular/names-of-enums-typescript/
+
+    Object.keys(enumType).filter((x) => Object.values(enumType).includes(x));
+
+/**
+ * Sleep function (used only for testing and simulating delays)
+ *
+ * The function can be called as:  await sleep(1000)  or  sleep(1000).then(...)
+ *
+ * @param ms sleep time in milliseconds
+ * @returns Empty Promise
+ */
+export const sleep = (ms: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+/**
+ * Implements the "nameof" operator.
+ *
+ * Usage:
+ *
+ * interface Person {
+ *    firstName: string;
+ *   lastName: string;
+ * }
+ *
+ * const personName = nameof<Person>("firstName");    //returns "firstName"
+ *
+ * Reference: https://schneidenbach.gitbooks.io/typescript-cookbook/content/nameof-operator.html
+ *
+ * @returns EnumListItems list
+ */
+export const nameofFactory = <T>() => (name: keyof T): keyof T => name;
+
+/**
  * Guest user ID constant.
  */
 export const GUEST_USER_ID = 0;
