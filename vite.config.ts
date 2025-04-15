70ab2f3dcaaf4c817e4cfb688fbcea7d2d9f0480
Jesus <jmlopezponce@gmail.com>
2025-04-15 08:49:34 -0400

    add path alias for demos in tsconfig.json; remove unused build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    // build: {
-        // rollupOptions: {
-        //     input: {
-        //         main: path.resolve(__dirname, 'index.html'),
-        //     },
-        //     output: {
-        //         manualChunks(id) {
-        //             if (id.includes('src/demos')) {
-        //                 return 'demos';
-        //             }
-        //         },
-        //     },
-        // },
-    // },
     plugins: [
         react(),
         tailwindcss(),
